66d685a658e78afd405db63dac4211a60530bcc1
Yael Harosh <yaelharosh01@gmail.com>
2023-04-01 08:49:17 -0400

    pass tests

--- lib/functions.py
+++ lib/functions.py
@@ -2,15 +2,26 @@
 
 def greet_programmer():
     pass
+    print('Hello, programmer!')
 
 def greet(name):
     pass
+    print(f'Hello, {name}!')
 
 def greet_with_default(name="programmer"):
     pass
+    print(f'Hello, {name}!')
 
 def add(num1, num2):
     pass
+    return num1 + num2;
 
 def halve(number):
     pass
+    return number / 2
+
+# def my_functions(param):
+#     print('Running my function')
+#     return param + 1
+
+# my_function_return_value = my_functions(1)
